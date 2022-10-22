874f2baae1ebd11a9b00cee6b944f316af411a29
dimavrb <dima---k@mail.ru>
2022-10-22 18:41:18 +0300

    Add GeckoDriver driver and Geckodriver path

--- conftest.py
+++ conftest.py
@@ -1,10 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default="en")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
@@ -14,7 +18,11 @@ def browser(request):
         browser = webdriver.Chrome('/Users/dmitrijkalinin/PycharmProjects/finaltaskinstepic/chromedriver')
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        options = Options()
+        firefox_profile = FirefoxProfile()
+        firefox_profile.set_preference("javascript.enabled", False)
+        options.profile = firefox_profile
+        browser = webdriver.Firefox(executable_path='/Users/dmitrijkalinin/PycharmProjects/finaltaskinstepic/geckodriver',options=options)
     else:
         print("Browser {} still is not implemented".format(browser_name))
     yield browser
